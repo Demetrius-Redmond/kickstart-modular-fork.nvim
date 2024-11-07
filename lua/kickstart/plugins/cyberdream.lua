2778ca714b7513cf172c6bb92082f90fe29cd1b2
Demetrius-Redmond <131230411+Demetrius-Redmond@users.noreply.github.com>
2024-11-07 16:59:52 -0600

    cyberdream test
    This is using the code I got from reddit

--- lua/kickstart/plugins/cyberdream.lua
+++ lua/kickstart/plugins/cyberdream.lua
@@ -3,5 +3,32 @@ return {
     'scottmckendry/cyberdream.nvim',
     lazy = false,
     priority = 1000,
+    opts = {
+      transparent = false,
+      italic_comments = true,
+      terminal_colors = true,
+      theme = {
+        variant = 'auto',
+      },
+    },
+    config = function(_, opts)
+      require('cyberdream').setup(opts)
+      require('cyberdream').load()
+    end,
+  },
+  {
+    '0xstepit/flow.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      fluo_color = 'orange',
+      mode = 'normal',
+      aggresive_spell = false,
+    },
+    config = function(_, opts)
+      require('flow').setup(opts)
+      require('flow').load()
+    end,
   },
 }
